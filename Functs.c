a28e7b7b66ea2d573399a3dbe96f63ab4900ed03
Jerimaine Hunt <100004627+JeriJamz@users.noreply.github.com>
2023-04-27 18:50:41 -0500

    Add files via upload

---
+++ Functs.c
@@ -0,0 +1,15 @@
+//its a crappy experiment
+int s,q,t,z;//god this got messy
+
+
+void int_swap(int x, int y){
+
+	*x = s;
+	*y = z;
+    &z = t;
+	s* = q;
+	t* =s;
+	s //I gotta stop lmao
+	//check byte order for the real func but I was trying to swap bits with just pointers
+	//and not Bitwise Operators. Lol It turned messy real quick
+}
