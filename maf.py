89d160e7749a3f148752290c0380593f146d5aed
Jerimaine Hunt <100004627+JeriJamz@users.noreply.github.com>
2024-04-15 16:41:55 -0500

    Add files via upload

---
+++ maf.py
@@ -0,0 +1,7 @@
+import math, random
+
+w ={x,y}#this is gonna be a shape
+
+x == y **3 *2
+
+y = randint(3,47) *3 #Try aand make this the Y Osc vice vERSfE
--- recurve.py
+++ recurve.py
@@ -1,21 +1,34 @@
-#this is an attempt at recursive I.E BufferOverFlow Check Wit 2 Compliment Addition
+#this is an attempt at recursive I.E BufferOverFlow Check
+#for my programmers this is becoming a security source code.
+#use as an regular import file edit and use at yo risk.
 import sys
-global x,y
-response = input(int(x))
-response1 = input(int(x))
-
-print("X is first then Y is Second\n\n\n\n\n",
-      "<Enter | Input>:")
 
 x = 0
 y = 0
 
-if x + y -2**32<= 2**32:
-    sys.exit()
-if 2**31 <=x+y:
-    sys.exit()
-if x+y == -2**31 <= x+y < 2**31:         
-    sys.flush()
-if x+y + 2**32 == x + y<-2**31:
-    sys.exit()
-    
+class RecurveForms:
+    def __init__(self, 2cAddition,usbAddition):
+        self.2cAddition
+        self.usbAddition
+
+
+
+    def 2cAddition(): 
+        if x + y -2**32>= 2**32:#postive overflow
+            print("BufferOverFlow")
+            sys.exit()
+        if 2**31 >=x+y:
+            print("BufferOverFlow")
+            sys.exit()
+        if x+y == -2**31 <= x+y < 2**31:         
+            sys.flush()
+        if x+y + 2**32 == x + y<-2**31:
+            print("BufferOverFlow")
+            sys.exit()
+
+        def usbAddition():
+            if x + y == x+y <2**32:
+                sys.flush
+            if x + y -2**32 and 2**32 <= x + y < 2**33:
+                print("OverFlow")
+                sys.exit
